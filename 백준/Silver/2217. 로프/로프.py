6573758f093472994bb3115614e30174fa519a68
jiyeoniing <156265339+jiyeoniing@users.noreply.github.com>
2024-03-15 00:27:57 +0900

    [Silver IV] Title: 로프, Time: 1956 ms, Memory: 35364 KB -BaekjoonHub

--- 백준/Silver/2217. 로프/로프.py
+++ 백준/Silver/2217. 로프/로프.py
@@ -1,14 +1,17 @@
+import sys
+input = sys.stdin.readline
 N = int(input())
 lst = []
-
 for _ in range(N):
     lst.append(int(input()))
 
 lst.sort()
 
-ans = []
-for x in lst:
-    ans.append(x*N)
-    N -= 1
+maxV = 0
+for _ in range(N):
+    x = lst.pop(0)
+    value = x*(len(lst)+1)
+    if maxV < value:
+        maxV = value
 
-print(max(ans))
+print(maxV)
