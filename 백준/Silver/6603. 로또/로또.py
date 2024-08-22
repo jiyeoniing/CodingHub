a53fbea89e6ab7ff67bf46f40798d58842978522
유지연 <apple20011215@gmail.com>
2024-08-22 14:19:37 +0900

    [Silver II] Title: 로또, Time: 120 ms, Memory: 112280 KB -BaekjoonHub

---
+++ 백준/Silver/6603. 로또/로또.py
@@ -0,0 +1,28 @@
+import sys
+
+input = sys.stdin.readline
+
+def combi(s, k):
+
+    if s==6:
+        print(*path)
+        return
+    
+    for i in range(k,K):
+
+        if not visited[i]:
+            path[s] = lotto[i]
+            visited[i] = True
+            combi(s+1, i+1)
+            visited[i] = False
+
+while True:
+    lst = list(map(int, input().split()))
+    K = lst[0]
+    if K==0:
+        break
+    lotto = lst[1:]
+    path = [0]*6
+    visited = [False]*K
+    combi(0, 0)
+    print()
