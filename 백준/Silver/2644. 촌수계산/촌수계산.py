088a83a1d8902dcb9c91d65db7f3f37d1732be27
유지연 <apple20011215@gmail.com>
2024-09-01 15:22:41 +0900

    [Silver II] Title: 촌수계산, Time: 88 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Silver/2644. 촌수계산/촌수계산.py
+++ 백준/Silver/2644. 촌수계산/촌수계산.py
@@ -1,44 +1,34 @@
-# 2644. 촌수계산
+# 촌수계산 - 실버 2
 
-# 부모 자식 : 1
-# 나 할아버지 : 2
-# 아버지형제 할아버지 : 1
-# 나 아버지형제 : 3
+# 2644
 
-# [[], [2, 3], [1, 7, 8, 9], [1], [5, 6], [4], [4], [2], [2], [2]] 
-def dfs(start, end, cnt):
-    global ans
+import sys
+input = sys.stdin.readline
 
-    if start == end:
-        ans = cnt
-        return
+n = int(input())
+a, b = map(int, input().split())
+graph = [ [] for _ in range(n+1) ]
+visited = [0]*(n+1)
 
-    for x in family[start]:
-        if not visited[x]:
-            visited[x] = True
-            dfs(x, end, cnt+1)
-            visited[x] = False
-
-
-people = int(input())
-family = [[] for _ in range(people+1)] # 이어진 관계 넣어줄 배열
-visited = [False]*(people+1) # 들렸으면 갈필요없음
-lst = list(map(int, input().split())) # 서로 다른 사람의 번호
-m = int(input()) # 관계의 개수
-
-# x , y = 부모의 번호, 나의 번호
+m = int(input())
 for _ in range(m):
-    (x, y) = map(int, input().split())
-    family[x].append(y)
-    family[y].append(x)
+    x,y = map(int, input().split())
+    graph[x].append(y)
+    graph[y].append(x)
 
-# print(family) # [[], [2, 3], [1, 7, 8, 9], [1], [5, 6], [4], [4], [2], [2], [2]]   
+# print(graph)
 
-ans = 0
-dfs(lst[0], lst[1], 0)
+def dfs(s, e, cnt):
 
-if ans == 0:
-    print(-1)
+    if visited[s]:
+        return
+    
+    visited[s] = cnt
 
-else:
-    print(ans)
+    for i in graph[s]:
+        dfs(i, e, cnt+1)
+
+
+dfs(a,b,1)
+# print(visited)
+print(visited[b]-1)
